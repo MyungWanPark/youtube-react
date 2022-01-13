feef6ec8e82042271a93fd7288f94fe72ca0aeac
MyungWanPark <mywanpark@gmail.com>
2022-01-13 13:24:24 +0900

    when video clicked, seperate screen into 7:3

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = ({ video: { snippet } }) => (
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        src={snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-        className={styles.thumbnail}
-      />
-      <div className={styles.metaData}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayStyle = display === "grid" ? styles.grid : styles.list;
+  return (
+    <li
+      className={`${styles.container} ${displayStyle}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+          className={styles.thumbnail}
+        />
+        <div className={styles.metaData}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default VideoItem;
