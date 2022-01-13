ee4bb9c96b6f69b501bdeaa36ba889ea65ccec31
MyungWanPark <mywanpark@gmail.com>
2022-01-13 13:44:20 +0900

    Add video player

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import styles from "./video_detail.module.css";
 
-const VideoDetail = ({ video }) => <h1>{video.snippet.title}</h1>;
+const VideoDetail = ({ video }) => {
+  return (
+    <section className={styles.video}>
+      <iframe
+        title={video.id}
+        type="text/html"
+        width="100%"
+        height="360"
+        src={`http://www.youtube.com/embed/${video.id}`}
+        frameBorder="0"
+      />
+      <h2>{video.snippet.title}</h2>
+      <h3>{video.snippet.channelTitle}</h3>
+      <pre className={styles.description}>{video.snippet.description}</pre>
+    </section>
+  );
+};
 
 export default VideoDetail;
